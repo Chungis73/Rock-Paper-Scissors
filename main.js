c4700abedd83772a6b549d7f450b5ebaf765a4f2
chungis73 <j.p.parker73@gmail.com>
2022-08-18 18:20:35 -0700

    made html js and css files

---
+++ main.js
@@ -0,0 +1,15 @@
+// Your game is going to play against the computer, so begin with a function 
+// called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’
+// or ‘Scissors’. We’ll use this function in the game to make the computer’s
+// play. Tip: use the console to make sure this is returning the expected output 
+// before moving to the next step!
+function getComputerChoice(Rock, Paper, Scissors){
+    let choice1 = (Rock)
+    let choice2 = (Paper)
+    let choice3 = (Scissors)
+
+    console.log(random, getComputerChoice[random]);
+}
+// Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - 
+// the playerSelection and computerSelection - snd then return a string that declares the winner of the round like so: 
+// "You Lose! Paper beats Rock"
