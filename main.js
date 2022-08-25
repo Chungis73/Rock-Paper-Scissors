0f080b33e0672255c930d1d53dbfcd5f5aa06065
chungis73 <j.p.parker73@gmail.com>
2022-08-24 21:48:35 -0700

    added some js

--- main.js
+++ main.js
@@ -3,13 +3,36 @@
 // or ‘Scissors’. We’ll use this function in the game to make the computer’s
 // play. Tip: use the console to make sure this is returning the expected output 
 // before moving to the next step!
-function getComputerChoice(Rock, Paper, Scissors){
-    let choice1 = (Rock)
-    let choice2 = (Paper)
-    let choice3 = (Scissors)
 
-    console.log(random, getComputerChoice[random]);
+const choices = ["rock","paper","scissors"]
+
+function getComputerChoice(){
+    return choices[Math.floor(Math.random() * choices.length)]
 }
+
 // Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - 
-// the playerSelection and computerSelection - snd then return a string that declares the winner of the round like so: 
-// "You Lose! Paper beats Rock"
+// the playerSelection and computerSelection - snd then return a string that declares the winner of the round 
+// like so: "You Lose! Paper beats Rock"
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        return "Draw"
+    }
+    else if
+        (playerSelection == "rock" && computerSelection == "scissors")||
+        (playerSelection == "paper" && computerSelection == "rock")||
+        (playerSelection == "scissors" && computerSelection == "paper"){
+        return `Winner! ${playerSelection} beats ${computerSelection}`
+    }
+    else{
+        return `Loser! ${computerSelection} beats ${playerSelection}`
+    }
+
+}
+
+// Write a NEW function called game(). Call the playRound function inside of this one to play 
+// a 5 round game that keeps score and reports a winner or loser at the end.
+
+function game(){
+
+}
