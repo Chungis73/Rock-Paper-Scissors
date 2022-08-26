3bafcc9b1f4c1d94cf976c0408fa28c3bf798452
chungis73 <j.p.parker73@gmail.com>
2022-08-26 00:36:07 -0700

    fixed issue with function running twice

--- main.js
+++ main.js
@@ -5,16 +5,14 @@
 // before moving to the next step!
 
 const choices = ["rock","paper","scissors"]
-
 function getComputerChoice(){
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
-console.log(getComputerChoice())
+let computerSelection = (getComputerChoice())
 // Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - 
 // the playerSelection and computerSelection - snd then return a string that declares the winner of the round 
 // like so: "You Lose! Paper beats Rock"
-
 function playRound(playerSelection, computerSelection){
     if (playerSelection == computerSelection){
         return "Draw"
@@ -31,13 +29,14 @@ function playRound(playerSelection, computerSelection){
         return `Loser! ${computerSelection} beats ${playerSelection}`
     }
 }
-const playerSelection = "rock"
-let computerSelection = getComputerChoice() 
-console.log(playRound(playerSelection, computerSelection))
+
 
 // Write a NEW function called game(). Call the playRound function inside of this one to play 
 // a 5 round game that keeps score and reports a winner or loser at the end.
 
 function game(){
-
+    for (i=1; i<6; i++){
+        console.log(playRound("rock",computerSelection))
+    }
 }
+game()
